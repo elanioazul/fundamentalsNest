4f3c399fe2f470b5bfa22d6e0189e80e8329e93b
hugo <hugo.herrador@gmail.com>
2023-01-28 18:44:06 +0100

    Use forFeature to register typeOrm in this child module

--- src/coffes/coffes.module.ts
+++ src/coffes/coffes.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CoffesController } from './coffes.controller';
 import { CoffesService } from './coffes.service';
+import { Coffe } from './entities/coffe.entity';
 
 @Module({
+    imports: [TypeOrmModule.forFeature([Coffe])],
     controllers: [CoffesController],
     providers: [CoffesService]
 })
