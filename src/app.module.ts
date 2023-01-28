33d5c0a523784abcfaa1b38bbef1ff521fe415c7
hugo <hugo.herrador@gmail.com>
2023-01-28 18:32:42 +0100

    Initialice typeOrm once set up docker-compose and app module match

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffesModule } from './coffes/coffes.module';
 
 @Module({
-  imports: [CoffesModule],
+  imports: [
+    CoffesModule, 
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost', // database host
+      port: 5435, //  port exposed 
+      username: 'postgres', // username
+      password: 'pass123', // user password (same in docker-compose file)
+      database: 'postgres', // name of our database,
+      autoLoadEntities: true, // models will be loaded automatically 
+      synchronize: true, // your entities will be synced with the database(recommended: disable in prod)
+    })
+  ],
   controllers: [AppController ],
   providers: [AppService],
 })
