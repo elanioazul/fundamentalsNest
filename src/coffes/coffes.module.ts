68d1fb3de4fa68d55af1897f4ec0514d94557771
hugo <hugo.herrador@gmail.com>
2023-02-04 11:23:43 +0100

    Understand value class based providers

--- src/coffes/coffes.module.ts
+++ src/coffes/coffes.module.ts
@@ -5,12 +5,13 @@ import { CoffesController } from './coffes.controller';
 import { CoffesService } from './coffes.service';
 import { Coffe } from './entities/coffe.entity';
 import { Flavor } from './entities/flavor.entity';
+
+class MockCoffeService {}
 @Module({
     imports: [TypeOrmModule.forFeature([Coffe, Flavor, Event])],
     controllers: [CoffesController],
     providers: [
-        CoffesService, 
-        { provide: 'COFFE_BRANDS', useValue: ['mocked', 'smoky'] }
+        { provide: CoffesService, useValue: new MockCoffeService }
     ],
     exports: [CoffesService]
 })
