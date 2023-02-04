fa7f9861bf3133a9bdb233868e8e1368811fef5f
hugo <hugo.herrador@gmail.com>
2023-02-04 12:10:17 +0100

    Usefactory sintax allows to create providers dinamically. Very usefull if u need to base the providers value on varios other dependencies, values... The value returned from the factory function is what will be used by the provider token

--- src/coffes/coffes.module.ts
+++ src/coffes/coffes.module.ts
@@ -1,20 +1,29 @@
-import { Module } from '@nestjs/common';
+import { Injectable, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Event } from 'src/events/entities/event.entity';
+import { COFFE_BRANDS } from './coffes.constants';
 import { CoffesController } from './coffes.controller';
 import { CoffesService } from './coffes.service';
 import { Coffe } from './entities/coffe.entity';
 import { Flavor } from './entities/flavor.entity';
 
-class MockCoffeService {}
-class prodConfigCoffeService {}
-class devConfigCoffeService {}
+@Injectable()
+export class CoffeBrandsFactory {
+  create() {
+    return ['mocked', 'bitter'];
+  }
+}
 @Module({
     imports: [TypeOrmModule.forFeature([Coffe, Flavor, Event])],
     controllers: [CoffesController],
     providers: [
-        { provide: CoffesService, useClass: process.env.NODE_ENV === 'development' ? devConfigCoffeService : prodConfigCoffeService}
-        // { provide: CoffesService, useValue: new MockCoffeService }
+        CoffesService,
+        CoffeBrandsFactory,
+        { 
+            provide: COFFE_BRANDS, 
+            useFactory: (brandsCoffe: CoffeBrandsFactory) => brandsCoffe.create(),
+            inject: [CoffeBrandsFactory]
+         }
     ],
     exports: [CoffesService]
 })
