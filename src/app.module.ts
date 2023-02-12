8f644f18561c2a569713b02bd5875446309813cf
hugo <hugo.herrador@gmail.com>
2023-02-12 11:26:59 +0100

    Make use of forRootAsync (async config). This instantiation will happen only after every other module registered in the app is resolved
    When ConfigModule(proccess.env) is resolved, then typeOrm config happens

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,20 +10,22 @@ import appConfig from './config/app.config';
 
 @Module({
   imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'postgres',
+        host: process.env.DATABASE_HOST,
+        port: +process.env.DATABASE_PORT,
+        username: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE_NAME,
+        autoLoadEntities: true, // models will be loaded automatically 
+        synchronize: true, // your entities will be synced with the database(recommended: disable in prod)
+      })
+    }), 
     ConfigModule.forRoot({
       load: [appConfig]
     }),
     CoffesModule, 
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DATABASE_HOST,
-      port: +process.env.DATABASE_PORT,
-      username: process.env.DATABASE_USER,
-      password: process.env.DATABASE_PASSWORD,
-      database: process.env.DATABASE_NAME,
-      autoLoadEntities: true, // models will be loaded automatically 
-      synchronize: true, // your entities will be synced with the database(recommended: disable in prod)
-    }), 
     CoffeRatingModule, DatabaseModule
   ],
   controllers: [AppController ],
