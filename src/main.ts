cfe483d674347ea3ac4b6172f807e5a5e5b0cb30
hugo <hugo.herrador@gmail.com>
2023-02-14 17:40:16 +0100

    Implement interceptor to wrapp all responsed in a data prop
    But can be used to manipulate requests and responsed

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 import { ApiKeyGuard } from './common/guards/api-key.guard';
+import { WrapResponseInterceptor } from './common/interceptors/wrap-response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,6 +15,7 @@ async function bootstrap() {
       enableImplicitConversion: true
     }
   }));
+  app.useGlobalInterceptors(new WrapResponseInterceptor())
   await app.listen(3000);
 }
 bootstrap();
