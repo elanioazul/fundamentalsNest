2659e1638ebd10b2be9cdfbf808954ac08616e1e
Hugo Herrador <hugo.herrador@geograma.com>
2023-02-15 13:39:11 +0100

    Swagger lift a url(localhost:3000/api) to see our services

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 import { ApiKeyGuard } from './common/guards/api-key.guard';
@@ -20,6 +21,13 @@ async function bootstrap() {
     new WrapResponseInterceptor(),
     //new TimeoutInterceptor()
   )
+  const options = new DocumentBuilder()
+  .setTitle('Ilovcoffee')
+  .setDescription('Coffee application')
+  .setVersion('1.0')
+  .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
