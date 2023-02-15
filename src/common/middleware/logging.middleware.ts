b8508c1e668066ef50815fcf972070631d41148e
Hugo Herrador <hugo.herrador@geograma.com>
2023-02-15 12:23:31 +0100

    Modify the greetings middleware to log how long every API call takes to complete

--- src/common/middleware/logging.middleware.ts
+++ src/common/middleware/logging.middleware.ts
@@ -3,8 +3,10 @@ import { Injectable, NestMiddleware } from '@nestjs/common';
 @Injectable()
 export class LoggingMiddleware implements NestMiddleware {
   use(req: any, res: any, next: () => void) {
+    console.time('Request-response time')
     console.log('Greetings from middleware');
     
+    res.on('finish', () => console.timeEnd('Request-response time'));
     next();
   }
 }
