ba9854c51296c2146ca6cbbddf84e79bd0191207
Hugo Herrador <hugo.herrador@geograma.com>
2023-02-15 15:39:21 +0100

    Add more control over swagger show up responses_2

--- src/coffes/coffes.controller.ts
+++ src/coffes/coffes.controller.ts
@@ -9,7 +9,7 @@ import {
 	Query,
 	SetMetadata
 } from "@nestjs/common";
-import { ApiResponse } from "@nestjs/swagger";
+import { ApiForbiddenResponse, ApiResponse } from "@nestjs/swagger";
 import { Protocol } from "src/common/decorators/protocol.decorator";
 import { Public } from "src/common/decorators/public.decorator";
 import { PaginationQueryDto } from "src/common/dto/pagination-query.dto";
@@ -23,7 +23,7 @@ import { UpdateCoffeeDto} from "./dto/update-coffee.dto";
 export class CoffesController {
 	constructor(private readonly coffesService: CoffesService){};
 
-	@ApiResponse({ status: 403, description: 'Forbidden. '})
+	@ApiForbiddenResponse({ description: 'Forbidden.' })
 	@Get()
 	@Public()
 	/*async*/ findAll(@Protocol('https') protocol: string, @Query() paginationQuery: PaginationQueryDto) {
